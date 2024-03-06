084d2d06bb3178fc433f1f4247241b16d8d09edd
dstorie80 <dstorie80@gmail.com>
2024-03-05 20:17:32 -0600

    Added files to the their folders

---
+++ server.js
@@ -0,0 +1,49 @@
+const path = require('path');
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const routes = require('./controllers');
+const helpers = require('./utils/helpers');
+
+//Sequelize Store
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const sequelize = require('./config/connection');
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Set up Handlebars.js with custom helpers
+const hbs = exphbs.create({ helpers });
+
+
+const sess = {
+    secret: 'Super secret secret',
+    cookie: { 
+    maxAge: 300000 , 
+    httpOnly: true,
+    secure: false,
+    sameSite: 'strict',
+    },
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+
+app.use(session(sess));
+
+// Tell express.js which template engine to use
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+//routes
+app.use(routes);
+
+// turn on connection to database and server
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now listening'));
+});
